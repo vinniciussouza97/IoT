31065e450e018bc08f5f9b23344a431c526c669b
vinniciussouza <vinniciussouza@yahoo.com.br>
2018-10-22 19:39:37 -0300

    post basico

---
+++ post_basico.py
@@ -0,0 +1,55 @@
+import time
+import requests
+import math
+import random
+
+TOKEN = "A1E-nkLIQeYE47RzHQ0ymL0jlDxBJWjwZk"  # Put your TOKEN here
+DEVICE_LABEL = "raspberry"  # Put your device label here 
+VARIABLE_LABEL_1 = "temperatura"  # Put your first variable label here
+
+def build_payload(variable_1):
+    # Creates two random values for sending data
+    value_1 = random.randint(0, 100)
+
+    payload = {variable_1: value_1}
+
+    return payload
+
+
+def post_request(payload):
+    # Creates the headers for the HTTP requests
+    url = "http://things.ubidots.com"
+    url = "{}/api/v1.6/devices/{}".format(url, DEVICE_LABEL)
+    headers = {"X-Auth-Token": TOKEN, "Content-Type": "application/json"}
+
+    # Makes the HTTP requests
+    status = 400
+    attempts = 0
+    while status >= 400 and attempts <= 5:
+        req = requests.post(url=url, headers=headers, json=payload)
+        status = req.status_code
+        attempts += 1
+        time.sleep(1)
+
+    # Processes results
+    if status >= 400:
+        print("[ERROR] Could not send data after 5 attempts, please check \
+            your token credentials and internet connection")
+        return False
+
+    print("[INFO] request made properly, your device is updated")
+    return True
+
+
+def main():
+    payload = build_payload(VARIABLE_LABEL_1)
+
+    print("[INFO] Attemping to send data")
+    post_request(payload)
+    print("[INFO] finished")
+
+
+if __name__ == '__main__':
+    while (True):
+        main()
+        time.sleep(1)
